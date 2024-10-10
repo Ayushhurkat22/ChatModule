9458501b6aeff32a0590f55b888d8f7181d9e6ab
AYUSH HURKAT <120328520+Ayushhurkat22@users.noreply.github.com>
2024-10-10 22:51:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const chatRoutes = require('./routes/chatRoutes');
 const app = express();
 const PORT = 5000;
 
+const cors = require('cors');
 app.use(cors());
+app.use(cors({
+  origin: 'https://chat-module-frontend.vercel.app/'
+}));
 app.use(express.json());
 
 app.use('/chat', chatRoutes);
